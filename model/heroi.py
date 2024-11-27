db04a06d82a9e308778eaf1cb672d1b360e3b8d9
Mari Coronado <maricoronad19@gmail.com>
2024-11-27 00:36:58 -0300

    update

--- model/heroi.py
+++ model/heroi.py
@@ -1,6 +1,5 @@
 class Heroi:
 
-    contador_id = 0
     lista_de_herois = []
     status_tornozeleira = False
     status_gps = False
@@ -16,8 +15,12 @@ class Heroi:
         self.convocado = convocado
         
         Heroi.lista_de_herois.append(self)
-        self.id =  Heroi.contador_id + 1
-        Heroi.contador_id += 1
+
+    def tornozeleira_formatada(self):
+        return "Sim" if self.status_tornozeleira else "Não"
+
+    def gps_formatado(self):
+        return "Sim" if self.status_gps else "Não"
 
     def imprimir_cabecalho():
         print(f'| {'Nome de Herói'.ljust(20)} | {'Nome Real'.ljust(20)} | {'Categoria'.ljust(12)} | {'Poderes'.ljust(30)} | {'Poder Principal'.ljust(20)} | {'Fraquezas'.ljust(20)} | {'Nível de Força'.ljust(15)} |')
@@ -26,22 +29,21 @@ class Heroi:
     def listar_vingadores(cls):
         Heroi.imprimir_cabecalho()
         for vingador in Heroi.lista_de_herois:
-            print(f'| {str(vingador.nome).ljust(20)} | {str(vingador.real).ljust(20)} | {str(vingador.categoria).ljust(12)} | {str(vingador.poderes).ljust(30)} | {str(vingador.poder_principal).ljust(20)} | {str(vingador.fraquezas).ljust(20)} | {int(vingador.forca)}{''*15} |')
+            print(f'| {str(vingador.nome).ljust(20)} | {str(vingador.real).ljust(20)} | {str(vingador.categoria).ljust(12)} | {str(vingador.poderes).ljust(30)} | {str(vingador.poder_principal).ljust(20)} | {str(vingador.fraquezas).ljust(20)} | {int(vingador.forca)}{' '*11} |')
 
     def __str__(self):
-        return f'| {str(self.nome).ljust(20)} | {str(self.real).ljust(20)} | {str(self.categoria).ljust(12)} | {str(self.poderes).ljust(30)} | {str(self.poder_principal).ljust(20)} | {str(self.fraquezas).ljust(20)} | {int(self.forca)}{''*15} |'
+        return f'| {str(self.nome).ljust(20)} | {str(self.real).ljust(20)} | {str(self.categoria).ljust(12)} | {str(self.poderes).ljust(30)} | {str(self.poder_principal).ljust(20)} | {str(self.fraquezas).ljust(20)} | {int(self.forca)}{' '*11} |'
 
     def lista_resumo():
-        print(f'| {'ID'} | {'Nome de Herói'.ljust(20)} |')
-        print(f'+{'-'*4}|{'-'*22}+')
+        print(f'| {'Nome de Herói'.ljust(20)} |')
+        print(f'+{'-'*22}+')
         for vingador in Heroi.lista_de_herois:
-            print(f'| {vingador.id}  | {str(vingador.nome).ljust(20)} |')
-        print(f'+{'-'*4}+{'-'*22}+')
-
+            print(f'| {str(vingador.nome).ljust(20)} |')
+        print(f'+{'-'*22}+')
 
     def lista_tornozeleira():
-        print(f'| {'Nome de Herói'.ljust(20)} | {'Nome Real'.ljust(20)} | {'Categoria'.ljust(15)} | {'Tornozeleira'.ljust(5)} | {'Localizador'.ljust(5)} |')
-        print(f'+{'-'*22}|{'-'*22}|{'-'*17}|{'-'*14}|{'-'*13}+')
-        for vingador in Heroi.lista_de_herois:
-            print(f'| {str(vingador.nome).ljust(20)} | {str(vingador.real).ljust(20)} | {str(vingador.categoria).ljust(15)} | {str(Heroi.status_tornozeleira).ljust(12)} | {str(Heroi.status_gps).ljust(11)} |')
-        print(f'+{'-'*22}|{'-'*22}|{'-'*17}|{'-'*14}|{'-'*13}+')
+        print(f"| {'Nome de Herói'.ljust(20)} | {'Nome Real'.ljust(20)} | {'Categoria'.ljust(15)} | {'Tornozeleira'.ljust(12)} | {'Localizador'.ljust(12)} |")
+        print(f"+{'-'*22}|{'-'*22}|{'-'*17}|{'-'*14}|{'-'*14}+")
+        for heroi in Heroi.lista_de_herois:
+            print(f"| {str(heroi.nome).ljust(20)} | {str(heroi.real).ljust(20)} | {str(heroi.categoria).ljust(15)} | {str(heroi.tornozeleira_formatada()).ljust(12)} | {str(heroi.gps_formatado()).ljust(12)} |")
+        print(f"+{'-'*22}|{'-'*22}|{'-'*17}|{'-'*14}|{'-'*14}+")
--- model/interface.py
+++ model/interface.py
@@ -4,18 +4,30 @@ import os
 
 class Interface:
 
-    # herois_convocados = []
     convocado = False
  
     def imprimir_titulo_app():
+        '''Uma função que imprime o título do app'''
         print('''
-▒█▀▀█ █▀▀█ █▀▀▄ █▀▀█ █▀▀ ▀▀█▀▀ █▀▀█ █▀▀█ 　 █▀▀▄ █▀▀ 　 ▒█░▒█ █▀▀ █▀▀█ █▀▀█ ░▀░ █▀▀
-▒█░░░ █▄▄█ █░░█ █▄▄█ ▀▀█ ░░█░░ █▄▄▀ █░░█ 　 █░░█ █▀▀ 　 ▒█▀▀█ █▀▀ █▄▄▀ █░░█ ▀█▀ ▀▀█
-▒█▄▄█ ▀░░▀ ▀▀▀░ ▀░░▀ ▀▀▀ ░░▀░░ ▀░▀▀ ▀▀▀▀ 　 ▀▀▀░ ▀▀▀ 　 ▒█░▒█ ▀▀▀ ▀░▀▀ ▀▀▀▀ ▀▀▀ ▀▀▀
-            ''')
+
+                                ░█████╗░░█████╗░██████╗░░█████╗░░██████╗████████╗██████╗░░█████╗░  ██████╗░░█████╗░░██████╗
+                                ██╔══██╗██╔══██╗██╔══██╗██╔══██╗██╔════╝╚══██╔══╝██╔══██╗██╔══██╗  ██╔══██╗██╔══██╗██╔════╝
+                                ██║░░╚═╝███████║██║░░██║███████║╚█████╗░░░░██║░░░██████╔╝██║░░██║  ██║░░██║██║░░██║╚█████╗░
+                                ██║░░██╗██╔══██║██║░░██║██╔══██║░╚═══██╗░░░██║░░░██╔══██╗██║░░██║  ██║░░██║██║░░██║░╚═══██╗
+                                ╚█████╔╝██║░░██║██████╔╝██║░░██║██████╔╝░░░██║░░░██║░░██║╚█████╔╝  ██████╔╝╚█████╔╝██████╔╝
+                                ░╚════╝░╚═╝░░╚═╝╚═════╝░╚═╝░░╚═╝╚═════╝░░░░╚═╝░░░╚═╝░░╚═╝░╚════╝░  ╚═════╝░░╚════╝░╚═════╝░
+
+                                        ██╗░░░██╗██╗███╗░░██╗░██████╗░░█████╗░██████╗░░█████╗░██████╗░███████╗░██████╗
+                                        ██║░░░██║██║████╗░██║██╔════╝░██╔══██╗██╔══██╗██╔══██╗██╔══██╗██╔════╝██╔════╝
+                                        ╚██╗░██╔╝██║██╔██╗██║██║░░██╗░███████║██║░░██║██║░░██║██████╔╝█████╗░░╚█████╗░
+                                        ░╚████╔╝░██║██║╚████║██║░░╚██╗██╔══██║██║░░██║██║░░██║██╔══██╗██╔══╝░░░╚═══██╗
+                                        ░░╚██╔╝░░██║██║░╚███║╚██████╔╝██║░░██║██████╔╝╚█████╔╝██║░░██║███████╗██████╔╝
+                                        ░░░╚═╝░░░╚═╝╚═╝░░╚══╝░╚═════╝░╚═╝░░╚═╝╚═════╝░░╚════╝░╚═╝░░╚═╝╚══════╝╚═════╝░
+''')
  
     @staticmethod
     def apresentar_menu_principal():
+        '''Uma função que apresenta o menu principal e as funções na qual o usuário pode escolher'''
         os.system('cls')
         Interface.imprimir_titulo_app()
         print('''        
@@ -36,63 +48,83 @@ class Interface:
 
     @staticmethod
     def imprime_titulo_tela(titulo):
+        '''Função com um pequeno título para cada uma das "guias" escolhidas'''
         os.system('cls')
         Interface.imprimir_titulo_app
         print(f'{str(titulo).upper()}')
         print('*' * 25)
         print()
 
+    @staticmethod
     def cadastrar_heroi():
+        '''Função que recolhe as informações e atributos de cada herói'''
         Interface.imprime_titulo_tela('Cadastrando novo herói')
         nome = input('Nome de herói: ')
         real = input('Nome real: ')
-        categoria = input('Categoria (humano, meta-humano, alienígena, deus): ')
+
+        while True: # Define os possíveis valores de categoria
+            try:
+                categoria = input('Categoria (humano, meta-humano, androide, alienígena, deidade): ')
+                if categoria.lower() not in ['humano', 'meta-humano', 'alienigena', 'deidade', 'androide']:
+                    print('\nEsta categoria não é válida, tente algo como: Humano, Meta-Humano, Androide, Alienigena ou Deidade')
+                else:
+                    break
+            except AttributeError:
+                print("Por favor, insira um valor válido.")
+
         poderes = input('Poderes: ')
+        if len(poderes) > 100:
+            print('Os poderes não podem exceder 100 caracteres. Tente novamente.')
+            return
+        
         poder_principal = input('Poder principal do herói: ')
         fraquezas = input('Fraquezas do herói: ')
-        forca = int(input('Nível de força (números inteiros): '))
+
+        while True:
+            try:
+                forca = int(input('Nível de força (0 - 10000): '))
+                if forca > 10000:
+                    print("O limite de força é 10000. Tente novamente.")
+                else:
+                    break
+            except ValueError:
+                print("Por favor, insira um número válido para o nível de força.")
+
         heroi = Heroi(nome, real, categoria, poderes, poder_principal, fraquezas, forca)
- 
         print(f'O Herói foi cadastrado: \n{heroi}')
 
+    @staticmethod
     def formar_time():
-        '''Uma função que seleciona vários heróis e forma um time, mostrando informações dos mesmos'''
+        '''Uma função que seleciona vários heróis pelo nome e forma um time, mostrando informações dos mesmos'''
         os.system('cls')
         Interface.imprime_titulo_tela('Formando um time!')
         Heroi.lista_resumo()
+    
         if Heroi.lista_de_herois:
             try:
-                id_escolhido = input('Escolha os IDs dos Heróis desejados (separando por vírgula ou espaço): ')
-                # Separa os IDs inseridos
-                id_escolhido = [id.strip() for id in id_escolhido.replace(',', ' ').split()]
+                nome_escolha = input('Digite o nome dos heróis para formar uma equipe (separados por vírgula ou espaço): ').strip().lower()
+                nomes = [nome.strip() for nome in nome_escolha.replace(',', ' ').split()]
 
-                time = []
+                time = []  # Lista para armazenar os heróis selecionados
 
-                for id_str in id_escolhido: # Verifica cada ID inserido
-                    try:
-                        id_escolhido = int(id_str) # Encontra o herói pelo ID
-                        heroi_encontrado = next((h for h in Heroi.lista_de_herois if h.id == id_escolhido), None)
+                for nome_str in nomes:
+                    herois_encontrados = next((h for h in Heroi.lista_de_herois if nome_str in h.nome.lower() or nome_str in h.real.lower()), None)
+                    if herois_encontrados:
+                        time.append(herois_encontrados)
+                    else:
+                        print(f'Herói com nome "{nome_str}" não encontrado.')
 
-                        if heroi_encontrado:
-                            time.append(heroi_encontrado)
-                        else:
-                            print(f'Herói com ID {id_escolhido} não encontrado.')
-
-                    except ValueError:
-                        print(f'O ID {id_str} é inválido. Por favor, insira números válidos!')
-
-                if time:
-                    Interface.imprime_titulo_tela('Equipe formada com sucesso!')
-                    Heroi.imprimir_cabecalho()
-                    for heroi in time:
-                        print(heroi)
-                else:
-                    print('Nenhum herói foi selecionado ou todos os IDs fornecidos foram inválidos.')
-
+                    if time:
+                        Interface.imprime_titulo_tela('Equipe formada com sucesso!')
+                        Heroi.imprimir_cabecalho()
+                        for heroi in time:
+                            print(heroi)
+                    else:
+                        print('Nenhum herói foi selecionado ou todos os nomes fornecidos foram inválidos.')
             except ValueError:
-                print("Por favor, insira um número válido para o ID.")
-                Interface.formar_time()
+                print("Houve um erro na entrada. Por favor, tente novamente.")    
 
+    @staticmethod
     def convocar_heroi():
         '''Uma função que filtra o nome do heroi e depois o convoca'''
         os.system('cls')
@@ -100,7 +132,7 @@ class Interface:
         if Heroi.lista_de_herois: 
             nome_escolha = input('Digite o nome do herói que deseja convocar: ').strip().lower()
 
-            heroi_encontrado = next((h for h in Heroi.lista_de_herois if nome_escolha.strip().lower() in h.nome.strip().lower()), None)
+            heroi_encontrado = next((h for h in Heroi.lista_de_herois if nome_escolha.strip().lower() in h.nome.strip().lower() or nome_escolha.strip().lower() in h.real.strip().lower()), None)
 
             if heroi_encontrado:
                 heroi_encontrado.convocado = True
@@ -110,53 +142,135 @@ class Interface:
         else:
             print('Nenhum herói cadastrado até o momento.')
 
+    @staticmethod
     def aplicar_tornozeleira():
+        '''Uma função que define se deve ser aplicada uma tornozeleira a determinado herói, faz a filtração dos nomes e define o valor da variavel como True (não é possível aplicar tornozeleira ao Thor, Hulk ou heróis com um dererminado nível de força...)'''
         os.system('cls')
         Interface.imprime_titulo_tela('Aplicação de Tornozeleira.')
+        
+        if not Heroi.lista_de_herois:
+            print("Não há heróis cadastrados no sistema.")
+            return
+        
         nome_escolha = input('Digite o nome do herói para aplicar a tornozeleira: ').strip()
-        heroi_encontrado = next((h for h in Heroi.lista_de_herois if nome_escolha.lower() in h.nome.lower()), None)
+        heroi_encontrado = next((h for h in Heroi.lista_de_herois if nome_escolha.strip().lower() in h.nome.strip().lower() or nome_escolha.strip().lower() in h.real.strip().lower()), None)
+
+        heroi_encontrado.forca = int(heroi_encontrado.forca)
 
         if heroi_encontrado is None:
             print("Nenhum herói encontrado com o nome informado.")
             return
-        if heroi_encontrado.convocado and heroi_encontrado.forca <= 500:
-            print(f"Uma tornozeleira foi aplicada ao herói {heroi_encontrado.nome}.")
-        elif heroi_encontrado.convocado and heroi_encontrado.forca > 500:
-            print(f"Não é possível aplicar uma tornozeleira a este herói por conta de seu nível de força ({heroi_encontrado.forca}).")
+        if not heroi_encontrado.convocado:
+            print(f"O herói {heroi_encontrado.nome} precisa ser convocado primeiro.")
+            return
+        
+        if not heroi_encontrado.status_tornozeleira: # se já não estiver com tornozeleira
+            if heroi_encontrado.nome.lower() == 'thor':
+                print('"O Deus do Trovão não se submeterá a tal dispositivo mundano!" - Não foi possível aplicar uma tornozeleira a Thor')
+            elif heroi_encontrado.nome.lower() == 'hulk':
+                print('"ARGH! HULK ESMAGA ESSE TROÇO!!!" - Não foi possível aplicar uma tornozeleira a Hulk')
+            else:
+                if heroi_encontrado.forca < 5000:
+                    heroi_encontrado.status_tornozeleira = True
+                    print(f"Uma tornozeleira foi aplicada ao herói {heroi_encontrado.nome}.")
+                else:
+                    print(f"Não é possível aplicar uma tornozeleira a {heroi_encontrado.nome} devido ao seu alto nível de força ({heroi_encontrado.forca}).")
+        else: 
+                print(f'O herói {heroi_encontrado.nome} já está usando uma tornozeleira.')   
+
+    @staticmethod
+    def aplicar_gps():
+        '''Uma função que define se deve ser aplicado um GPS a determinado herói, faz a filtração dos nomes e define o valor da variavel como True'''
+        os.system('cls')
+        Interface.imprime_titulo_tela('Aplicação de Localizador.')
+        
+        if not Heroi.lista_de_herois:
+            print("Não há heróis cadastrados no sistema.")
+            return
+        
+        nome_escolha = input('Digite o nome do herói para aplicar o GPS: ').strip()
+        heroi_encontrado = next((h for h in Heroi.lista_de_herois if nome_escolha.strip().lower() in h.nome.strip().lower() or nome_escolha.strip().lower() in h.real.strip().lower()), None)
+        if heroi_encontrado is None:
+            print("Nenhum herói encontrado com o nome informado.")
+            return
+        if not heroi_encontrado.status_tornozeleira:
+            print(f"O herói {heroi_encontrado.nome} precisa estar usando uma tornozeleira primeiro.")
+            return
         else:
-            print("O herói precisa ser convocado primeiro.")
+            if not heroi_encontrado.status_gps:
+                heroi_encontrado.status_gps = True
+                print(f'O GPS da tornozeleira do herói {heroi_encontrado.nome} foi ligado.')
+            else:
+                print(f'O herói {heroi_encontrado.nome} já está sendo rastreado.')
 
-        # id_heroi = input('Digite o nome do herói para aplicar a tornozeleira: ').strip().lower()
+    @staticmethod
+    def emitir_mandado():
+        '''Emite um mandado para um heróis especifico'''
+        os.system('cls')
+        Interface.imprime_titulo_tela('Emissão do mandado de prisão.')
 
-        # heroi_encontrado = next((h for h in Heroi.lista_de_herois if str(h.id) == id_heroi), None)
+        nome_escolha = input('Digite o nome do herói para emitir um mandado de prisão: ').strip()
+        heroi_encontrado = next((h for h in Heroi.lista_de_herois if nome_escolha.strip().lower() in h.nome.strip().lower() or nome_escolha.strip().lower() in h.real.strip().lower()), None)
 
-        # if heroi_encontrado:
-        #     if heroi_encontrado.convocado:
-        #         print(f'A tornozeleira foi aplicada ao herói {heroi_encontrado.nome}.')
-        #     else:
-        #         print(f'O herói {heroi_encontrado.nome} não foi convocado. Não é possível aplicar a tornozeleira.')
-        # else:
-        #     print(f'Nenhum herói encontrado com o nome {id_heroi}.')
+        print(f'Um mandado de prisão foi emitido para {heroi_encontrado.nome}.')
 
+    @staticmethod
+    def info():
+        '''Mostra todas as informações de um vingador'''
+        os.system('cls')
+        Interface.imprime_titulo_tela('Emissão do mandado de prisão.')
+
+        nome_escolha = input('Digite o nome do herói para obter suas informações: ').strip()
+        heroi_encontrado = next((h for h in Heroi.lista_de_herois if nome_escolha.strip().lower() in h.nome.strip().lower() or nome_escolha.strip().lower() in h.real.strip().lower()), None)
+        print(f'''
+Nome do herói: {heroi_encontrado.nome}
+Nome real: {heroi_encontrado.real}
+Categoria: {heroi_encontrado.categoria}
+Poderes: {heroi_encontrado.poderes}
+Poder principal: {heroi_encontrado.poder_principal}
+Fraquezas: {heroi_encontrado.forca}
+Nível de força: {heroi_encontrado.forca}
+Status de convocação: {heroi_encontrado.convocado}
+Status da tornozeleira: {heroi_encontrado.status_tornozeleira}
+Status do chip GPS: {heroi_encontrado.status_gps}
+''')
+        
     def ler_opcao_usuario():
         '''Uma função que lê a opção do usuário na tela inicial'''
         try:
             opcao = int(input('O que deseja fazer?: '))
             if opcao == 1:
+                Interface.imprimir_titulo_app()
                 Interface.cadastrar_heroi()
             elif opcao == 2:
+                Interface.imprimir_titulo_app()
                 Interface.imprime_titulo_tela('Listando Vingadores')
                 Heroi.lista_tornozeleira()
             elif opcao == 3:
+                Interface.imprimir_titulo_app()
                 Interface.imprime_titulo_tela('Formando um time!')
                 print('Escolha Heróis para formar um time: ')
                 Interface.formar_time()
             elif opcao == 4:
+                Interface.imprimir_titulo_app()
                 Interface.imprime_titulo_tela('Convocando Herói!')
                 Interface.convocar_heroi()
             elif opcao == 5:
+                Interface.imprimir_titulo_app()
                 Interface.imprime_titulo_tela('Aplicando Tornozeleira!')
                 Interface.aplicar_tornozeleira()
+            elif opcao == 6:
+                Interface.imprimir_titulo_app()
+                Interface.imprime_titulo_tela('Aplicando Localizador!')
+                Interface.aplicar_gps()
+            elif opcao == 7:
+                Interface.imprimir_titulo_app()
+                Interface.imprime_titulo_tela('Emitindo mandado de prisão!')
+                Interface.emitir_mandado()
+            elif opcao == 8:
+                Interface.imprimir_titulo_app()
+                Interface.imprime_titulo_tela('Informações sobre o Herói!')
+                Interface.info()
             elif opcao == 0:
                 print('Encerrando programa')
                 exit()
