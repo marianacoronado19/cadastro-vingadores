3d67c85dcfccb52ba786089a668fa764343eab59
marianacoronado19 <maricoronad19@gmail.com>
2025-01-28 13:23:31 -0300

    build: instalando o flask

--- app.py
+++ app.py
@@ -1,7 +1,20 @@
-from model.interface import Interface
+from flask import Flask, render_template
 
-def main():
-    Interface()
+app = Flask(__name__) #__name__ = variavel que representa o nome da classe
 
-if __name__ == '__main__': # Sempre no final do arquivo contendo a definição da função principal
-    main()
+@app.route("/") 
+def home():
+    return render_template("home.html")
+
+@app.route("/ola")
+def pagina_inicial():
+    return "<p>Olá, mundo!</p>"
+
+
+# from model.interface import Interface
+
+# def main():
+#     Interface()
+
+# if __name__ == '__main__': # Sempre no final do arquivo contendo a definição da função principal
+#     main()
